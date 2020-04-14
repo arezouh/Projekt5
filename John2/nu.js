bf8cb1d321182b0455578e886cc97f3e6340cda9
arezouh <arez0109@edu.ucl.dk>
2020-04-14 19:47:19 +0200

    Alt

---
+++ John2/nu.js
@@ -0,0 +1,15 @@
+function calc() {
+  var a = 5;
+  var b = 5;
+  var c = 4;
+  var d = 5;
+  var e = 6;
+  var f = 5;
+
+  var calculate;
+ {
+    calculate = a + b + c + d + e + f;
+  }
+
+document.querySelector("#result").innerHTML = calculate;
+}
